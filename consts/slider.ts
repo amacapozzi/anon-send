04e9dae36b838f2af0dd6eb77e791dc7cf0fe16b
amacapozzi <amacapozzi07@gmail.com>
2025-04-10 17:04:27 -0300

    feat: add sliderImages constant for image slider functionality

---
+++ consts/slider.ts
@@ -0,0 +1,5 @@
+export const sliderImages = [
+  "https://images.unsplash.com/photo-1504333638930-c8787321eee0?q=80&w=2070",
+  "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?q=80&w=2111&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1516331138075-f3adc1e149cd?q=80&w=2108&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
