3222cddad2223151ca09773d80f19513ee4a99f8
amacapozzi <amacapozzi07@gmail.com>
2025-04-10 17:31:56 -0300

    refactor: improve signUpSchema structure with refined password matching validation

--- schemas/auth.ts
+++ schemas/auth.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
 
-export const signUpSchema = z.object({
-  alias: z.string().min(3, { message: "Alias is required" }),
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters long" }),
-  confirmPassword: z
-    .string()
-    .min(1, { message: "Confirm password is required" }),
-});
+export const signUpSchema = z
+  .object({
+    alias: z.string().min(3, { message: "Alias is required" }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Confirm password is required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
