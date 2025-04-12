bf1742f8902d79b5e6cd7480c29f33fa0523261a
amacapozzi <amacapozzi07@gmail.com>
2025-04-12 12:29:12 -0300

    feat: add new Page component for category-based mail layout

---
+++ app/mail/[category]/page.tsx
@@ -0,0 +1,15 @@
+import MailLayout from "@/layouts/mail-layout";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ category: string }>;
+}) {
+  const { category } = await params;
+
+  return (
+    <MailLayout>
+      <div>a</div>
+    </MailLayout>
+  );
+}
