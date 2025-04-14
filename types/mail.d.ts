c3662525b1257008ec178eca76fcdf0df0db288b
amacapozzi <amacapozzi07@gmail.com>
2025-04-14 20:53:44 -0300

    feat: define SendMessageData interface for message sending functionality

--- types/mail.d.ts
+++ types/mail.d.ts
@@ -2,3 +2,9 @@ import { mailSchema } from "@/schemas/mail";
 import { z } from "zod";
 
 export type Mail = z.infer<typeof mailSchema>;
+
+export interface SendMessageData {
+  title: string;
+  message: string;
+  file: File | null;
+}
