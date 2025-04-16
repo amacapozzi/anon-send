6ebac5893ad310ba83673ce8c6a2aef4ea204451
amacapozzi <amacapozzi07@gmail.com>
2025-04-15 22:40:45 -0300

    feat: implement open dialog functionality in AppSidebarOpenMessage component

--- components/app-sidebar-open-message.tsx
+++ components/app-sidebar-open-message.tsx
@@ -1,15 +1,24 @@
+"use client";
 import { MessageCircleIcon } from "lucide-react";
+import { useState } from "react";
+import { SendMessageDialog } from "./dialogs/send-message-dialog";
 
 export const AppSidebarOpenMessage = () => {
+  const [isOpenDialog, setIsOpenDialog] = useState(false);
+
+  const handleOpenDialog = () => {
+    setIsOpenDialog(true);
+  };
+
   return (
     <a href="#">
-      <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+      <div
+        onClick={handleOpenDialog}
+        className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
+      >
         <MessageCircleIcon className="size-4" />
       </div>
-      <div className="grid flex-1 text-left text-sm leading-tight">
-        <span className="truncate font-semibold">Acme Inc</span>
-        <span className="truncate text-xs">Enterprise</span>
-      </div>
+      <SendMessageDialog open={isOpenDialog} />
     </a>
   );
 };
