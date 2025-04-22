320eababc6cd82a0da5e59097fa39ee538b437ac
amacapozzi <amacapozzi07@gmail.com>
2025-04-21 22:54:01 -0300

    refactor: reorder import statement and enhance type definitions in mail types

--- types/mail.d.ts
+++ types/mail.d.ts
@@ -1,5 +1,5 @@
-import { mailSchema } from "@/schemas/mail";
 import { z } from "zod";
+import { mailSchema } from "@/schemas/mail";
 
 export type Mail = z.infer<typeof mailSchema>;
 
@@ -9,3 +9,38 @@ export interface SendMessageData {
   body: string;
   file: File | null;
 }
+
+export type MailData = {
+  id: string;
+  subject: string;
+  body: string;
+  sender: {
+    id: string;
+    password: string;
+    alias: string;
+    publicProfile: boolean;
+    createdAt: Date;
+    avatarUrl: string | null;
+  } | null;
+  read: boolean;
+  createdAt: Date;
+};
+
+export type Attachment = {
+  id: string;
+  name: string;
+  size: string;
+};
+
+export type EmailMessage = {
+  id: string;
+  subject: string;
+  content: string;
+  timestamp: string;
+  sender: {
+    name: string;
+    email: string;
+    avatar: string | null;
+  };
+  attachments: Attachment[];
+};
