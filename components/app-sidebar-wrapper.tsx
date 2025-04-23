6d98efbad3b30b63a264d8ddd7decae777ac040b
amacapozzi <amacapozzi07@gmail.com>
2025-04-22 22:33:06 -0300

    feat: update mail retrieval to use getMailsByCategory in AppSidebarWrapper

--- components/app-sidebar-wrapper.tsx
+++ components/app-sidebar-wrapper.tsx
@@ -1,11 +1,11 @@
-import { getInboxMails } from "@/actions/mail";
+import { getMailsByCategory } from "@/actions/mail";
 import { AppSidebar } from "@/components/app-sidebar";
 import { getCurrentUserServer } from "@/lib/auth";
+import { getCurrentPathFromCookie, getHeaderByName } from "@/lib/headers";
 import { SessionUser } from "@/types/session";
 
 export default async function AppSidebarWrapper() {
-  const mails = await getInboxMails();
-
+  const mails = await getMailsByCategory();
   const user = await getCurrentUserServer();
 
   return <AppSidebar user={user as SessionUser} initialMails={mails} />;
