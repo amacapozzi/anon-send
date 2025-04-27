932bd98624f1766e6e158331cc14b82dd3665482
amacapozzi <amacapozzi07@gmail.com>
2025-04-27 20:02:58 -0300

    feat: refactor MailPage to use Mail component and improve layout handling

--- app/mail/chat/[id]/page.tsx
+++ app/mail/chat/[id]/page.tsx
@@ -1,26 +1,49 @@
-import MailLayout from "@/layouts/mail-layout";
+import Image from "next/image";
+
+import { accounts, mails } from "@/consts/data";
+import { cookies } from "next/headers";
 import { getMailById } from "@/actions/mail";
-import ChatUI from "@/components/chat";
-import { getCurrentUserServer } from "@/lib/auth";
-import Chat from "@/components/chat";
-import { User } from "@prisma/client";
+import { Mail } from "@/components/mail";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const mails = await getMailById(id);
-  const user = await getCurrentUserServer();
+export default async function MailPage({ params }: { params: { id: string } }) {
+  const c = await cookies();
 
-  console.log(mails);
+  const mailId = params.id;
+
+  const mails = await getMailById(mailId);
+
+  const layout = c.get("react-resizable-panels:layout:mail");
+  const collapsed = c.get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
 
   return (
-    <MailLayout>
-      <div className="flex-1 overflow-y-auto">
-        <ChatUI user={user as User} initialMessages={mails ?? []} />
-      </div>{" "}
-    </MailLayout>
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/mail-dark.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="hidden dark:block"
+        />
+        <Image
+          src="/examples/mail-light.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="block dark:hidden"
+        />
+      </div>
+      <div className="hidden flex-col md:flex">
+        <Mail
+          initialMails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+    </>
   );
 }
