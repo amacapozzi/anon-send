c382d7ea37abbd22963797bc852bd1479dea8e9e
amacapozzi <amacapozzi07@gmail.com>
2025-04-27 20:02:49 -0300

    feat: add MailList component for displaying a list of emails

---
+++ components/mail-list.tsx
@@ -0,0 +1,59 @@
+import { ComponentProps } from "react";
+import { formatDistanceToNow } from "date-fns/formatDistanceToNow";
+
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { type Message } from "@/types/message";
+
+interface MailListProps {
+  initialMails: Message[];
+}
+
+export function MailList({ initialMails }: MailListProps) {
+  return (
+    <ScrollArea className="h-screen">
+      <div className="flex flex-col gap-2 p-4 pt-0">
+        {initialMails.map((item) => (
+          <button
+            key={item.id}
+            className={cn(
+              "flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent"
+            )}
+          >
+            <div className="flex w-full flex-col gap-1">
+              <div className="flex items-center">
+                <div className="flex items-center gap-2">
+                  <div className="font-semibold">{item.subject}</div>
+                </div>
+                <div className="ml-auto text-xs text-muted-foreground">
+                  {formatDistanceToNow(new Date(item.createdAt), {
+                    addSuffix: true,
+                  })}
+                </div>
+              </div>
+              <div className="text-xs font-medium">{item.subject}</div>
+            </div>
+            <div className="line-clamp-2 text-xs text-muted-foreground">
+              {item.body.substring(0, 300)}
+            </div>
+          </button>
+        ))}
+      </div>
+    </ScrollArea>
+  );
+}
+
+function getBadgeVariantFromLabel(
+  label: string
+): ComponentProps<typeof Badge>["variant"] {
+  if (["work"].includes(label.toLowerCase())) {
+    return "default";
+  }
+
+  if (["personal"].includes(label.toLowerCase())) {
+    return "outline";
+  }
+
+  return "secondary";
+}
